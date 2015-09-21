48f8775f43c91d83aff2371411dc1627ea7b73aa
bondparkerbond <parkerbond@gmail.com>
2015-09-21 15:59:53 -0600

    Basic jQuery into materialize card

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -10,114 +10,116 @@ $(document).ready( function() {
 				for(index in data.users) {
 					var user = data.users[index];
 					$('#users').append("<li class='person' data-user-id='" + user.id + "' data-first-name='" + user.first_name + "'>" + user.first_name + ' ' + user.last_name + ' ' + user.phone_number + "<button class='delete'>Delete</button><button id='show'>Show</button></li>");
-				}
-			},
-			error: function(data) {
+          $('#user_details').append("<span class='card-title'>" + user.first_name + ' ' + user.last_name + "</span><p>" + user.phone_number + "</p>");
+        }
+      },
+      error: function(data) {
 
-			}
-		});
-	});
+      }
+    });
+  });
 
-	function showUser(user) {
-		$.ajax('/get_user', {
-			type: 'GET',
-			data: user,
-			success: function(data) {
-				$('body').html(data);
-			}
-		});
-	}
+  function showUser(user) {
+    $.ajax('/get_user', {
+      type: 'GET',
+      data: user,
+      success: function(data) {
+        $('body').html(data);
+      }
+    });
+  }
 
-	$(document).on('click', '#show', function() {
-		var id = $(this).closest('.person').data('user-id');
-		$.ajax(baseUrl + id, {
-			type: 'GET',
-			success: function(data) {
-				showUser(data.user);
-			},
-			error: function(data) {
-			}
-		});
-	});
+  $(document).on('click', '#show', function() {
+    var id = $(this).closest('.person').data('user-id');
+    $.ajax(baseUrl + id, {
+      type: 'GET',
+      success: function(data) {
+        showUser(data.user);
+      },
+      error: function(data) {
+      }
+    });
+  });
 
 
-	$(document).on('click', '.person', function() {
-		$('#edit_user').removeClass('hide');
-		$.ajax(baseUrl + $(this).data('user-id'), {
-			type: 'GET',
-			success: function(data) {
-				$('#first_name').val(data.user.first_name);
-				$('#last_name').val(data.user.last_name);
-				$('#phone_number').val(data.user.phone_number);
-				$('.edit').data('user-id', data.user.id);
-			}
-		});
-	});
+  $(document).on('click', '.person', function() {
+    $('#edit_user').removeClass('hide');
+    $.ajax(baseUrl + $(this).data('user-id'), {
+      type: 'GET',
+      success: function(data) {
+        $('#first_name').val(data.user.first_name);
+        $('#last_name').val(data.user.last_name);
+        $('#phone_number').val(data.user.phone_number);
+        $('.edit').data('user-id', data.user.id);
+      }
+    });
+  });
 
-	$('.edit').on('click', function() {
-		var first = $('#first_name').val();
-		var last = $('#last_name').val();
-		var phone = $('#phone_number').val();
-		var user = { user: {first_name: first, last_name: last, phone_number: phone }};
-		$.ajax(baseUrl + $(this).data('user-id'),
-		{
-			type: 'PUT',
-			data: user,
-			success: function(data) {
-				$('#edit_user').addClass('hide');
-			}
-		});
-	});
+  $('.edit').on('click', function() {
+    var first = $('#first_name').val();
+    var last = $('#last_name').val();
+    var phone = $('#phone_number').val();
+    var user = { user: {first_name: first, last_name: last, phone_number: phone }};
+    $.ajax(baseUrl + $(this).data('user-id'),
+    {
+      type: 'PUT',
+      data: user,
+      success: function(data) {
+        $('#edit_user').addClass('hide');
+      }
+    });
+  });
 
-	$(document).on('click', '.delete', function() {
-		var id = $(this).closest('.person').data('user-id');
-		var that = $(this);
-		$.ajax(baseUrl + id, {
-			type: 'DELETE',
-			success: function(data) {
-				that.closest('.person').remove();
-			}  // line 82 or 80
-		});  // line 79 and? 80
-	});   // line 76
+  $(document).on('click', '.delete', function() {
+    var id = $(this).closest('.person').data('user-id');
+    var that = $(this);
+    $.ajax(baseUrl + id, {
+      type: 'DELETE',
+      success: function(data) {
+        that.closest('.person').remove();
+      }
+    });
+  });
 
-	function compare(a,b) {
-		if (a.first_name < b.first_name)
-			return -1;
-		if (a.first_name >b.first_name)
-			return 1;
-		return 0;
-	}
+  function compare(a,b) {
+    if (a.first_name < b.first_name)
+      return -1;
+    if (a.first_name >b.first_name)
+      return 1;
+    return 0;
+  }
 
-	$('#add_sort').on('click', function() {
-		var people = $('.person');
-		var sortarr = [];
-		for (var i = 0; i < people.length; i++) {
-			sortarr.push({first_name: people[i].dataset.firstName, id: people[i].dataset.userId});
-		}
+  $('#add_sort').on('click', function() {
+    var people = $('.person');
+    var sortarr = [];
+    for (var i = 0; i < people.length; i++) {
+      sortarr.push({first_name: people[i].dataset.firstName, id: people[i].dataset.userId});
+    }
     
-		var newarr = sortarr.sort(compare);
-		console.log(newarr);
-		$('#users').empty()
-		for(index in newarr) {
-			var user = newarr[index];
-			$('#users').append("<li class='person' data-user-id='" + user.id + "'>" + user.first_name + "<button class='delete'>Delete</button><button id='show'>Show</button></li>");
-		}
-		$('#add_sort').addClass('hide')
-	});
+    var newarr = sortarr.sort(compare);
+    console.log(newarr);
+    $('#users').empty()
+    for(index in newarr) {
+      var user = newarr[index];
+      $('#users').append("<li class='person' data-user-id='" + user.id + "'>" + user.first_name + "<button class='delete'>Delete</button><button id='show'>Show</button></li>");
+    }
+    $('#add_sort').addClass('hide')
+  });
 
 	$('.add').on('click', function(){
 		var firstName = $('#new_first_name').val();
 		var lastName = $('#new_last_name').val();
 		var phoneNumber = $('#new_phone_number').val();
 		var user = { user: {first_name: firstName, last_name: lastName, phone_number: phoneNumber }};
-		//moved 3 variables to the top, will be renaming them
+		// can't move 3 variables to the top unless they are put
+    // into a new function and called using the new function...
+    // see userdryup.js inside images folder for example!
 		$.ajax(baseUrl, {
 			type: 'POST',
 			data: user,
 			success: function(data) {
 				var user = data.user;
 				$('#users').append("<li class='person' data-user-id='" + user.id + "'>" + user.first_name + "<button class='delete'>Delete</button></li>");
-
 			}
 		});
 	});
